c20903b0631c79783955cf3052b5dcbc52a84643
Anushkawebron <anushka@weberon.net>
2024-02-16 11:59:21 +0530

    "react-spring animation"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Gsap_animation from './Gsap_animation';
+import React_spring_animation from './React_spring_animation';
 
 function App() {
   
   return(
     <>
       <Gsap_animation/>
+      <React_spring_animation/>
     </>
   )
 }
--- src/React_spring_animation.js
+++ src/React_spring_animation.js
@@ -1,1 +1,36 @@
-import { animated } from '@react-spring/web'
+import { useSpring,animated } from '@react-spring/web'
+function React_spring_animation() {
+    const image = useSpring({
+        from: {scale: 1.5},
+        to: {scale: 1},
+        loop: true,
+        config:{ duration: 5000, }
+    })
+    const text = useSpring({
+        from: {scale: 0.5},
+        to: {scale: 1},
+        loop: true,
+        config:{ duration: 5000,}        
+    })
+  
+    return(
+      <>
+        <h6> React spring animation</h6>
+      <div className="container">
+        <div className="header">
+          <animated.div style={image} className="backgroundImage"></animated.div>
+          <div className="headerContent">
+            <div className="image">
+              <img src='/image/praveen.png'/>
+            </div>
+            <div className="text">
+              <animated.div style={text} >Beat out other buyers to Hot New Listings</animated.div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </>
+    )
+  }
+  
+  export default React_spring_animation;
